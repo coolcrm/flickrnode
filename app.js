35914314551a3929ee56dd4adedfbec6eac9803d
coolcrm <crmrussia@gmail.com>
2013-10-05 22:30:48 +0400

    5 oct 2013

--- app.js
+++ app.js
@@ -12,6 +12,44 @@ var api = flickr(
 */
 
 var fullpath = '/home/vk/Загрузки/Fisher3.jpg';
+
+extraslist = "tags='dog,cat'";
+// usually, the method name is precisely the name of the API method, as they are here:
+    api({method: 'flickr.photos.getRecent', extras: extraslist}, function(err, response) {
+	console.log('Total:', response.photos.total);      
+	console.log('Response:', response.photos.photo[10].id);
+	console.log('Response:', response.photos.photo[10].secret);
+	console.log('Response:', response.photos.photo[10].server);
+	console.log('Response:', response.photos.photo[10].farm);
+	//var text = JSON.stringify(response);
+	//console.log('Stringified:', text);
+	//var obj=JSON.parse(response);
+	//console.log('Parcing:', obj.photos);
+      //api({method: 'flickr.photosets.addPhoto', photoset_id: 1272356126, photo_id: new_photo_id}, function(err, response) {
+      //  console.log('Added photo to photoset:', response);
+      //});
+	//console.log(construct_photo_url("001", "002", "003", "004"));
+	var photourl;
+	var i = 10;
+	photourl = construct_photo_url(response.photos.photo[i].farm, response.photos.photo[i].server, response.photos.photo[i].id, response.photos.photo[10].secret);
+	console.log(photourl);
+	//
+	file = fs.createWriteStream("./yu/file.jpg");
+	var request = http.get(photourl, function(response) {response.pipe(file);});
+    });
+
+//We create photo URL
+//Using this article
+//https://secure.flickr.com/services/api/misc.urls.html
+//And this format
+//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
+function construct_photo_url(farm, server, id, secret){
+var url="http://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_b.jpg"
+return url;
+
+}
+
+
 // the upload method is special, but this library automatically handles the
 // hostname change
 /*
@@ -40,38 +78,3 @@ api({
   }
 });
 */
-extraslist = "tags='dog,cat'";
-// usually, the method name is precisely the name of the API method, as they are here:
-    api({method: 'flickr.photos.getRecent', extras: extraslist}, function(err, response) {
-	console.log('Total:', response.photos.total);      
-	console.log('Response:', response.photos.photo[10].id);
-	console.log('Response:', response.photos.photo[10].secret);
-	console.log('Response:', response.photos.photo[10].server);
-	console.log('Response:', response.photos.photo[10].farm);
-	//var text = JSON.stringify(response);
-	//console.log('Stringified:', text);
-	//var obj=JSON.parse(response);
-	//console.log('Parcing:', obj.photos);
-      //api({method: 'flickr.photosets.addPhoto', photoset_id: 1272356126, photo_id: new_photo_id}, function(err, response) {
-      //  console.log('Added photo to photoset:', response);
-      //});
-	//console.log(construct_photo_url("001", "002", "003", "004"));
-	var photourl;
-	var i = 10;
-	photourl = construct_photo_url(response.photos.photo[i].farm, response.photos.photo[i].server, response.photos.photo[i].id, response.photos.photo[10].secret);
-	console.log(photourl);
-	//
-	file = fs.createWriteStream("file.jpg");
-	var request = http.get(photourl, function(response) {response.pipe(file);});
-    });
-
-//We create photo URL
-//Using this article
-//https://secure.flickr.com/services/api/misc.urls.html
-//And this format
-//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-function construct_photo_url(farm, server, id, secret){
-var url="http://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_b.jpg"
-return url;
-
-}
