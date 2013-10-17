2d019f5edb7d5c410e397537c83e207c5138bd94
Vladimir Karagioz <crmrussia@gmail.com>
2013-10-17 20:58:41 +0400

    Finally Working

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var response_copy;
 
 var api = flickr(
   '63b283b748057fcd5fdcd9d67a7dda75', // consumer_key
-  '557b871d824b2c65c')/*   , // consumer_secret
+  '557b871d824b2c65c');/*   , // consumer_secret
   '3XF0pqP4daZf9oIlx-a7H1uMLeGrBidkJU', // oauth_token
   'KpslBxHoh4QYk6ad'); // oauth_token_secret
 
@@ -32,21 +32,22 @@ async.series([
 	if ( err ) {
         // handle the error safely
         console.log('If was error of api!');
+	return callback(err);
     	}
 	else{
 	console.log('If was not a error!');
 	//debugger;
-	/*
+	
 	console.log('Total:', response.photos.total);      
 	console.log('Response:', response.photos.photo[10].id);
 	console.log('Response:', response.photos.photo[10].secret);
 	console.log('Response:', response.photos.photo[10].server);
 	console.log('Response:', response.photos.photo[10].farm);
-	*/
+	
 	debugger;
 	response_copy = response;
 	
-	for (i=1; i<10;i++){
+	for (i=0; i<10;i++){
 	console.log("i="+i);
 		photourl [i] = construct_photo_url(response_copy.photos.photo[i].farm, response_copy.photos.photo[i].server, response_copy.photos.photo[i].id, response_copy.photos.photo[i].secret);
 	console.log(photourl[i]);
@@ -54,9 +55,11 @@ async.series([
 	} //End of for	
 	} //End of else
 	debugger;
-	});//End of API Callback
+	callback();
+	}
+	);//End of API Callback
 	
-    	callback();
+    	
 	},
     function(callback) {
 	debugger; 
@@ -66,16 +69,30 @@ async.series([
 	
 	//filename= "./images/file"+i+".jpg";
 	//console.log(filename);
-	
+	/*
 	require('async').eachLimit(photourl, 5, function(url, next){
 	  download(url, "./images/file"+(i++)+".jpg", next);
-		}, function(){
-		   console.log('finished');
+		}, function(err){
+			if (err) return next(err);
+		   	console.log('finished');
 		})
-
+	*/
 	callback();
 
-}]);
+}],
+	function(err) { //This function gets called after the two tasks have called their "task callbacks"
+        //if (err) return err; //If an error occured
+	//console.log("Final callback");
+	//filename= "./images/file"+i+".jpg";
+	//console.log(filename);
+	debugger;
+	require('async').eachLimit(photourl, 5, function(url, next){
+	  download(url, "./images/file"+(i++)+".jpg", next);
+		}, function(err){
+			if (err) return next(err);
+		   	console.log('finished');
+		})
+    });
 }//End of function getfiles
 
 var download = function(url, dest, cb) {
@@ -91,6 +108,7 @@ var download = function(url, dest, cb) {
 
 
 //We create photo URL
+
 //Using this article
 //https://secure.flickr.com/services/api/misc.urls.html
 //And this format
